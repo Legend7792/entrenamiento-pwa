b42979328bfb79da28238f7d7d52f85ab4827bdc
Legend7792 <leiniermoreno4@gmail.com>
2026-01-20 08:55:03 -0500

    Mensaje de commit

--- sw.js
+++ sw.js
@@ -9,14 +9,25 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
+});
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
